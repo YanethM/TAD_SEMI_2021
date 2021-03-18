e2c7369297c7b7e16d33363be9ed3891d75001b5
YanethM <YanethM@users.noreply.github.com>
2021-03-18 16:53:25 -0500

    Add files via upload

---
+++ Listas simples v2/js/singleLinkedList.js
@@ -0,0 +1,188 @@
+const capturaValorInputAnonima = function() {
+    let valoresNodos = document.getElementById("nodos").value;
+    document.getElementById("nodosIngresados").innerHTML = valoresNodos;
+};
+
+/* const capturaValorInputFlecha = () => {
+    let valoresNodos = document.getElementById("nodos").value;
+    document.getElementById("nodosIngresados").innerHTML = valoresNodos;
+};
+
+function funcionEstandar() {
+    let valoresNodos = document.getElementById("nodos").value;
+    document.getElementById("nodosIngresados").innerHTML = valoresNodos;
+} */
+
+
+class NodeClass {
+
+    constructor(valor) {
+        this.valor = valor;
+        this.next = null;
+    }
+}
+
+class listasSimples {
+    constructor() {
+            this.head = null;
+            this.tail = null;
+            this.length = 0;
+        }
+        /* Métodos de la lista: añadir, eliminar, buscar, actualizar valor */
+    añadirNodoF(valor) {
+        /* Instancia de la clase NodeClass */
+        let newNode = new NodeClass(valor);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    añadirNodoI(valor) {
+        let newNode = new NodeClass(valor);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    eliminarNodoF() {
+        if (!this.head) return undefined;
+        let nodoVisitado = this.head;
+        let nuevaColaLista = nodoVisitado;
+        while (nodoVisitado.next) {
+            nuevaColaLista = nodoVisitado;
+            nodoVisitado = nodoVisitado.next;
+        }
+        this.tail = nuevaColaLista;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return nodoVisitado;
+    }
+    eliminarNodoI() {
+        if (!this.head) return undefined;
+        let cabezaactual = this.head;
+        this.head = cabezaactual.next;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return cabezaactual;
+    }
+
+    getPosicionPuntero(index) {
+        if (index < 0 || index >= this.length) return null;
+        let contadorPuntero = 0;
+        let nodoVisitado = this.head;
+        while (contadorPuntero !== index) {
+            nodoVisitado = nodoVisitado.next;
+            contadorPuntero++;
+        }
+        return nodoVisitado;
+    }
+
+    modificarValorNodo(index, valor) {
+        let encontrarNodo = this.getPosicionPuntero(index);
+        if (encontrarNodo) {
+            encontrarNodo.valor = valor;
+            return true;
+        }
+        return false;
+    }
+
+    removerNodoPorPosicion(index) {
+        let nodoVisitad = this.head;
+        let nodoAnteriorAlVisitado = null;
+        if (index < 0 || index >= this.length) return null;
+        if (index === 0) this.head = nodoVisitad.next;
+        else {
+            for (let i = 0; i < index; i++) {
+                nodoAnteriorAlVisitado = nodoVisitad;
+                nodoVisitad = nodoVisitad.next;
+            };
+            nodoAnteriorAlVisitado.next = nodoVisitad.next;
+        };
+        this.length--;
+        return nodoVisitad.valor;
+    }
+
+    insertarNodoPorPosicion(valor, index) {
+        let newNode = new NodeClass(valor);
+        let nodoVisitado = this.head;
+        let nodoAnteriorAlVisitado;
+        if (index < 0 || index >= this.length) return null;
+        if (index === 0) this.añadirNodoI(valor);
+        else {
+            for (let i = 0; i < index; i++) {
+                nodoAnteriorAlVisitado = nodoVisitado;
+                nodoVisitado = nonodoVisitado.next;
+            }
+            newNode.next = nodoVisitado;
+            nodoAnteriorAlVisitado.next = newNode;
+        }
+        this.length++;
+    }
+    removerNodoPorValor(valor) {
+        let nodoVisitado = this.head;
+        let nodoAnteriorAlVisitado = null;
+        while (nodoVisitado !== null) {
+            if (nodoVisitado.valor === valor) {
+                if (!nodoAnteriorAlVisitado)
+                    this.head = nodoVisitado.next;
+                else
+                    nodoAnteriorAlVisitado.next = nodoVisitado.next;
+                this.length--;
+                return nodoVisitado.valor;
+            }
+            nodoAnteriorAlVisitado = nodoVisitado;
+            nodoVisitado = nodoVisitado.next;
+        }
+        return null;
+    }
+
+    imprimirArrayList() {
+        let arregloNodos = [];
+        let nodoVisitado = this.head;
+        while (nodoVisitado) {
+            arregloNodos.push(nodoVisitado.valor);
+            nodoVisitado = nodoVisitado.next;
+        }
+        document.getElementById("listaPorDefault").innerHTML = arregloNodos;
+    }
+}
+let instClass = new listasSimples();
+instClass.añadirNodoF(5);
+instClass.añadirNodoI(4);
+instClass.añadirNodoI(3);
+instClass.añadirNodoI(2);
+instClass.añadirNodoI(1);
+instClass.añadirNodoI(7);
+instClass.añadirNodoI(8);
+instClass.añadirNodoI(9);
+
+console.log(instClass);
+/* instClass.eliminarNodoI();
+instClass.eliminarNodoF(); 
+instClass.modificarValorNodo(1, "Dos");*/
+instClass.removerNodoPorPosicion(1); /* Elimina nodo con valor 3 */
+console.log(instClass);
+
+instClass.insertarNodoPorPosicion("Nuevo nodo", 0);
+instClass.removerNodoPorValor(3);
+
+instClass.imprimirArrayList();
